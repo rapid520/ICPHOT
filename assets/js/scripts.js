10d95edf8a601479baadbeb9613e5cc05df273df
rapid520 <1401171837@qq.com>
2022-09-30 19:42:32 +0800

    666

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,53 +1,40 @@
-
 jQuery(document).ready(function() {
-	
+
     /*
         Fullscreen background
     */
     $.backstretch("assets/img/backgrounds/1.jpg");
-    
-    $('#top-navbar-1').on('shown.bs.collapse', function(){
-    	$.backstretch("resize");
+
+    $('#top-navbar-1').on('shown.bs.collapse', function() {
+        $.backstretch("resize");
     });
-    $('#top-navbar-1').on('hidden.bs.collapse', function(){
-    	$.backstretch("resize");
+    $('#top-navbar-1').on('hidden.bs.collapse', function() {
+        $.backstretch("resize");
     });
-    
+
     /*
 	    Contact form
 	*/
-	$('.contact-form form input[type="text"], .contact-form form textarea').on('focus', function() {
-		$('.contact-form form input[type="text"], .contact-form form textarea').removeClass('input-error');
-	});
-	$('.contact-form form').submit(function(e) {
-		e.preventDefault();
-	    $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('input-error');
-	    var postdata = $('.contact-form form').serialize();
-	    $.ajax({
-	        type: 'POST',
-	        url: 'assets/contact.php',
-	        data: postdata,
-	        dataType: 'json',
-	        success: function(json) {
-	            if(json.emailMessage != '') {
-	                $('.contact-form form .contact-email').addClass('input-error');
-	            }
-	            if(json.subjectMessage != '') {
-	                $('.contact-form form .contact-subject').addClass('input-error');
-	            }
-	            if(json.messageMessage != '') {
-	                $('.contact-form form textarea').addClass('input-error');
-	            }
-	            if(json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
-	                $('.contact-form form').fadeOut('fast', function() {
-	                    $('.contact-form').append('<p>Thanks for contacting us! We will get back to you very soon.</p>');
-	                    // reload background
-	    				$.backstretch("resize");
-	                });
-	            }
-	        }
-	    });
-	});
-    
-    
+    $('.contact-form form input[type="text"], .contact-form form textarea').on('focus', function() {
+        $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('input-error');
+    });
+    $('.contact-form form').submit(function(e) {
+        e.preventDefault();
+        $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('input-error');
+        var postdata = $('.contact-form form').serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'https://icp.9longpo.cn/1.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(json) {
+                if (json) {
+                    alert("We have received your message and will check it as soon as possible")
+                    $("form")[0].reset();
+                }
+            }
+        });
+    });
+
+
 });
